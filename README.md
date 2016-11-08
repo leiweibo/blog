493c79d294fc9569ba7f2b4d073ed3b46b7da8de
Lei,Weibo <leiweibo@gmail.com>
2016-11-08 23:59:37 +0800

    add form support

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ It's was developed base on what I read from the book [Oreilly.Flask.Web.Developm
 
 #####Steps:
 * pip install flask-bootstrap
+* pip install flask-moment
+* pip install flask-wtf
 
 [1]:http://shop.oreilly.com/product/0636920031116.do
--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, make_response, render_template
 import os
+
 from flask.ext.bootstrap import Bootstrap
+
+from flask.ext.moment import Moment
+from datetime import datetime
+
+from flask.ext.wtf import Form 
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required])
+    submit = SubmitField('Submit')
 
 template_path = os.path.abspath('./templates')
 app = Flask(__name__, template_folder=template_path)
+app.config['SECRET_KEY'] = 'hard to guess string' ###TODO
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -19,7 +34,7 @@ def index():
     # response = make_response('<h1> This document carries a cookie!</h1>')
     # response.set_cookie('answer', '42')
     # return response
-    return render_template('index.html')
+    return render_template('index.html', form=NameForm())
 
 @app.route('/user/<name>')
 def user(name):
