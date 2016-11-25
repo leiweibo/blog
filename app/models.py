f0bf250d02ece8ddc3b48e80ffb617f7dccec062
Lei,Weibo <leiweibo@gmail.com>
2016-11-26 04:40:15 +0800

    add basic support for the user roles

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin, current_user
+from flask_login import UserMixin, current_user, AnonymousUserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
@@ -15,6 +15,14 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default=False)
 
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+    
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -84,8 +92,24 @@ class User(UserMixin, db.Model):
         db.session.commit()
         return True
 
+    def can(self, permissions):
+        # print('abc')
+        return self.role is not None and (self.role.permissions & permissions) == permissions
+
+    def is_aministrator(self):
+        return self.can(Permission.ADMINISTER)
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser        
 
 class Role(db.Model):
     __tablename__ ='roles'
--- tests/test_user_models.py
+++ tests/test_user_models.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import User
+from app.models import User, Role, Permission, AnonymousUser
 
 class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
@@ -19,5 +19,15 @@ class UserModelTestCase(unittest.TestCase):
     def test_password_random(self):
         u1 = User(password = 'cat')
         u2 = User(password = 'cat')
-        self.assertTrue(u1.password_hash != u2.password_hash)        
+        self.assertTrue(u1.password_hash != u2.password_hash)    
+
+    def test_roles_and_permissons(self):
+        Role.insert_role()
+        u = User(email='leiweibo@gmail.com', password = 'cat')
+        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+        self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
+
+    def test_anoynymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
                     
