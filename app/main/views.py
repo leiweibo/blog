ac45f3fa0a39407791555ce6ee089d4f19a6a5d5
Lei,Weibo <leiweibo@gmail.com>
2016-11-27 16:39:38 +0800

    we can post blog now

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, session, redirect, url_for, current_app
+from flask_login import current_user
 from .. import db
-from ..models import User, Permission
+from ..models import Post, Permission
 from ..email import send_email
 from . import main
 from .forms import PostForm
@@ -9,19 +10,10 @@ from .forms import PostForm
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = PostForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
+    if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
+        post = Post(body = form.body.data, author=current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
         return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts = posts)
