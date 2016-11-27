cff2ed639369066247d0649b557800e1370bc66e
Lei,Weibo <leiweibo@gmail.com>
2016-11-27 22:37:08 +0800

    add pagination support for the post list

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, request
 from flask_login import current_user
 from .. import db
 from ..models import Post, Permission
@@ -15,5 +15,10 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts = posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out = False
+      )
+    posts = pagination.items
+    return render_template('index.html', form=form, posts = posts, pagination=pagination)
--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'leiweibo@gmail.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = 10
 
     @staticmethod
     def init_app(app):
