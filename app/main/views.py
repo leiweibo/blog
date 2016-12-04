2dd2f20f6da16829f032a4d38cf0dc1534d87947
Lei,Weibo <leiweibo@gmail.com>
2016-12-04 22:03:50 +0800

    add comment list under the post page

--- app/main/views.py
+++ app/main/views.py
@@ -52,18 +52,19 @@ def show_followed():
 @main.route('/post/<int:id>', methods=['Get', 'Post'])
 def post(id):
     post = Post.query.get_or_404(id)
-    form = CommonetForm()
+    form = CommentForm()
     if form.validate_on_submit():
-        comment = Comment(body=form.body.data, post = posts, author = current_user._get_current_object())
+        comment = Comment(body=form.body.data, post = post, author = current_user._get_current_object())
         db.session.add(comment)
         db.session.commit()
         return redirect(url_for('.post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
     if page == -1:
         page = (post.comments.count() -1) / current_app.config['FLASKY_COMMENTS_PER_PAGE']
-    pagination = post.comments.order_by(Comment.timestamp.asc()).pagination(
+    pagination = post.comments.order_by(Comment.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'], error_out=False)
     comments = pagination.items
-    return render_template('post.html', posts=[post], form=form, comments = commnets, pagination = pagination)
+    return render_template('post.html', posts=[post], form=form, comments = comments, pagination = pagination)
 
 @main.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
