3198ce0c74ccd27de30d6b893baa20edec8d925b
Lei,Weibo <leiweibo@gmail.com>
2016-12-10 23:55:55 +0800

    update some code base on the github flasky reposity

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -1,27 +1,10 @@
-from flask.ext.httpauth import HTTPBasicAuth
-auth = HTTPBasicAuth()
+from flask import g, jsonify
+from flask_httpauth import HTTPBasicAuth
+from ..models import User, AnonymousUser
+from . import api
 from .errors import forbidden_error
 
-@auth.verify_password
-def verify_password(email, password):
-  if email == '':
-    g.current_user = AnonymousUser()
-    return True
-  user = User.query.filter_by(email = email).first()
-  if not user:
-    return False
-  g.current_user = user
-  return user.verify_password(password)
-
-@auth.error_handler
-def auth_error():
-  return unauthorized('Invalid credentials')
-
-@api.before_request
-@auth.login_required
-def before_request():
-  if not g.current_user.is_anoymous and not g.current_user.confirmed:
-    return forbidden('Uncofirmed account')
+auth = HTTPBasicAuth()
 
 @auth.verify_password
 def verify_password(email_or_token, password):
@@ -38,6 +21,18 @@ def verify_password(email_or_token, password):
   g.current_user = user
   g.token_used = False
   return user.verify_password(password)
+
+@auth.error_handler
+def auth_error():
+  return unauthorized('Invalid credentials')
+
+@api.before_request
+@auth.login_required
+def before_request():
+  if not g.current_user.is_anoymous and not g.current_user.confirmed:
+    return forbidden('Uncofirmed account')
+
+
 
 @api.route('/token')
 def get_token():
