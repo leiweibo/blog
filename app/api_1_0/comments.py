318571bf630a1c5f937f2fb752f96d1bd6486068
Lei,Weibo <leiweibo@gmail.com>
2016-12-12 22:54:27 +0800

    add comment end-point

--- app/api_1_0/comments.py
+++ app/api_1_0/comments.py
@@ -27,6 +27,34 @@ def get_comments():
     'count': pagination.total
   })
 
+@api.route('/comments/<int:id>')
+def get_comment(id):
+  comment = Comment.query.get_or_404(id)
+  return jsonify(comment.to_json())
+
+@api.route('/posts/<int:id>/comments/')
+def get_post_comments(id):
+  post = Post.query.get_or_404(id)
+  page = request.args.get('page', 1, type=int)
+  pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+      page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+      error_out = False
+    )
+  comments = pagination.items
+  prev = None
+  if pagination.has_prev:
+    prev = url_for('api.get_post_comments', id = id, page = page - 1, _external = True)
+  next = None
+  if pagination.has_next:
+    next = url_for('api.get_post_comments', id = id, page = page + 1, _external = True)
+
+  return jsonify({
+      'comments': [comment.to_json() for comment in comments],
+      'prev': prev, 
+      'next': next,
+      'count': pagination.total
+    })
+
 @api.route('/posts/<int:id>/comments/', methods=['POST'])
 @permission_required(Permission.COMMENT)
 def new_post_comment(id):
